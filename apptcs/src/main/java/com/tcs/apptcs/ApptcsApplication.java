590cc302553275d3bfc52e17ccc3c199223f4c89
Francisco <franciscovaldez86@gmail.com>
2022-04-15 19:56:47 -0500

    Commit inicial con Eclipse. Close #1

---
+++ apptcs/src/main/java/com/tcs/apptcs/ApptcsApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.apptcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptcsApplication.class, args);
+	}
+
+}
