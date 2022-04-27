417f7f561d8e78aaca65213fb297923dc47a62d5
Francisco Valdez <87194400+Franciscoxvl@users.noreply.github.com>
2022-04-26 19:20:05 -0500

    Add files via upload

--- apptcs/src/main/java/com/tcs/apptcs/ApptcsApplication.java
+++ apptcs/src/main/java/com/tcs/apptcs/ApptcsApplication.java
@@ -2,9 +2,17 @@ package com.tcs.apptcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApptcsApplication {
+public class ApptcsApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ApptcsApplication.class);
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApptcsApplication.class, args);
